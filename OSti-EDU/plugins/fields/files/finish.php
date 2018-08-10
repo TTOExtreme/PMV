ffec26641fb35581bde261f908ff150cb63b0627
TTOExtreme <camarottol@hotmail.com>
2018-08-10 07:56:27 -0300

    initial OSTI-EDU

---
+++ OSti-EDU/plugins/fields/files/finish.php
@@ -0,0 +1,61 @@
+<?php
+
+	if (empty($_GET["id"])) {
+		$_GET["id"] = "";
+	}
+	include '_Connect.php';
+
+	$DB = Connect();
+		LoadCss();
+	if(!empty($DB) && empty($_GET["solve"]) && empty($_GET["_id"])){
+		GetPageId($_GET["id"]);
+	}else{
+		if(!empty($_GET["solve"]) && !empty($_GET["_id"])){
+			Solve($_GET["_id"]);
+		}else{
+			echo "Preencha todos os campos";
+		}
+	}
+
+	function GetPageId($_id){
+		$DB = Connect();
+		$query = "SELECT * FROM ".$GLOBALS['table']." WHERE id='".$_id."';";
+		$result = $DB->query($query);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				echo "<center><form method='get' target='returnframe'><table style='max-width:480px; width:480px;'>";
+				echo "<tr style='display:none'><td><input type='text' name='_id' value='{$_id}'></td></tr>";
+				echo "<tr><td><textarea class='input' type='text' name='solve' placeholder='Solução:'></textarea></td></tr>";
+				echo "<tr><td><button class='input' type='submit' formaction='http://{$_SERVER['HTTP_HOST']}/OSti/plugins/fields/files/finish.php'>Salvar</button>";
+				echo "</table></form></center>";
+				echo "<iframe name='returnframe' style='border:none;'></iframe>";//<iframe name="returnframe" style="display: none;"></iframe>
+			}
+		}
+	}
+
+	function Solve($_id){
+		$DB = Connect();
+		//UPDATE $table SET `ranking` = `ranking`-1 WHERE plugin_fields_containers_id = {$_POST['container_id']}
+		$query = "UPDATE ".$GLOBALS['table']." SET solution='".$_GET['solve']."' WHERE id='".$_id."';";
+		$result = $DB->query($query);
+		//print_r($result);
+		$query = "UPDATE ".$GLOBALS['table']." SET status='6' WHERE id='".$_id."';";
+		$result = $DB->query($query);
+		print_r($result);
+	}
+
+	function LoadCss(){
+		echo"
+		<style type='text/css'>
+			td,table{
+				border: solid 1px black; 
+			}
+			.input{
+				width:100%;
+			}
+
+		</style>
+		";
+	}
+
+?>
---
+++ OSti-EDU/plugins/fields/files/print_page.php
@@ -0,0 +1,91 @@
+<?php
+
+	if (empty($_GET["id"])) {
+		$_GET["id"] = "";
+	}
+	include '_Connect.php';
+
+	$DB = Connect();
+	if(!empty($DB)){
+		LoadCss();
+		GetPageId($_GET["id"]);
+	}
+
+	function GetPageId($_id){
+		$DB = Connect();
+		$query = "SELECT * FROM ".$GLOBALS['table']." WHERE id='".$_id."';";
+		$result = $DB->query($query);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				echo "<html moznomarginboxes mozdisallowselectionprint><body onload='window.print()'>";
+				echo "<table style='max-width:480px; width:480px;'>";
+				echo "<tr><td colspan='3'>Numero da OS: {$row['id']}</td></tr>";
+				echo "<tr><td colspan='3'>Encarregado: ".GetTech($row['id'])."</td></tr>";
+				echo "<tr><td colspan='3'>Data de emissão: {$row['date_creation']}</td></tr>";
+				echo "<tr><td colspan='3'>Emissor: ".GetUser($row['users_id_recipient'])."</td></tr>";
+				echo "<tr><td colspan='3'>Problema: {$row['content']}</td></tr>";
+				echo "<tr><td>Urgencia: {$row['urgency']}</td>";
+				echo "<td>Impacto: {$row['impact']}</td>";
+				echo "<td>Prioridade: {$row['priority']}</td></tr>";
+				echo "<tr><td colspan='3'>Localizaçao: ".GetLocation($row['locations_id'])."</td></tr>";
+				echo "</table></br></body></html>";
+				
+				
+			}
+		}
+	}
+
+	function GetLocation($_id){
+		$DB = Connect();
+		$query = "SELECT * FROM ".$GLOBALS['dbname'].".glpi_locations WHERE id='".$_id."';";
+		$result = $DB->query($query);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				return $row['name'];
+			}
+		}
+	}
+	function GetUser($_id){
+		$DB = Connect();
+		$query = "SELECT * FROM ".$GLOBALS['dbname'].".glpi_users WHERE id='".$_id."';";
+		$result = $DB->query($query);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				if($row['firstname']!=""){
+					return $row['firstname']." ".$row['realname'];
+				}else{
+					return $row['name'];
+				}
+			}
+		}
+	}
+	function GetTech($_id){
+		$DB = Connect();
+		$query = "SELECT * FROM ".$GLOBALS['dbname'].".glpi_tickets_users WHERE tickets_id='".$_id."' AND type='2';";
+		$result = $DB->query($query);
+		//print_r($result);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				return GetUser($row['users_id']);
+			}
+		}
+	}
+	function LoadCss(){
+		echo"
+		<style type='text/css'>
+			td,table{
+				border: solid 1px black; 
+			}
+			@media print {
+				@page { 
+					margin-top:4mm;
+					margin-bottom:0mm;
+			  	}
+			  body { margin: 0; }
+			}
+
+		</style>
+		";
+	}
+
+?>
