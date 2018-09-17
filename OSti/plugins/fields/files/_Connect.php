86bae2ebd433245b88c6000a3e69dcb7feb71bcc
TTOExtreme <camarottol@hotmail.com>
2018-09-17 09:46:50 -0300

    fix Last  staus and conection with sql

--- OSti/plugins/fields/files/_Connect.php
+++ OSti/plugins/fields/files/_Connect.php
@@ -2,10 +2,10 @@
   	function Connect(){
 		//$servername = $_SERVER['HTTP_HOST'];
 		$servername = "localhost";
-		$username = "glpi";
-		$password = "tipmv2018";
-		$GLOBALS['dbname'] = "osti_pmv";
+		$username = "OSTI-pmv";
+		$password = "osti@pmv";
+		$GLOBALS['dbname'] = "PMV_OSTI_PMV";
 		$GLOBALS['table'] = $GLOBALS['dbname'].".glpi_tickets";
 		return new mysqli($servername, $username, $password, $GLOBALS['dbname']);
 	}
-?>
+?> 
